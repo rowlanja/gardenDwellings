c5cbbb1be60324245ab642e6f7dea34fb3a7ec49
j r <rowlanja@tcd.ie>
2024-02-11 17:02:05 +0000

    Removed leaking CSS

--- amplifyapp/src/pages/Contact.js
+++ amplifyapp/src/pages/Contact.js
@@ -1,9 +1,43 @@
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
 function Contact() {
+   const initialState = {name: '', email: '', message: ''}
+   const [eachEntry, setEachEntry] = useState(initialState)
+   const {name, email, message} = eachEntry
+   const [validated, setValidated] = useState(false);
+
+   const handleSubmit = (event) => {
+     const form = event.currentTarget;
+     console.log('Submitting data');
+     if (form.checkValidity() === false) {
+       event.preventDefault();
+       event.stopPropagation();
+     }
+ 
+     setValidated(true);
+   };
 
    return ( 
-      <h1>
-      
-      </h1>
+      <div className='App'>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicName">           
+            <Form.Label>Name</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+         <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control/>
+         </Form.Group>
+         
+         <Form.Group controlId="formBasicTextField">
+            <Form.Label>Message</Form.Label>
+            <Form.Control/>
+        </Form.Group>
+        <Button variant="success" type="submit">Submit</Button>
+      </Form>
+   </div>
    );
 };
   
